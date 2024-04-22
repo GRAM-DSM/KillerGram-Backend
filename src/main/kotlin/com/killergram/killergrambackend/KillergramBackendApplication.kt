6da16a8799b3ce762cc35e95d51d4647c761a7d5
jyk1029 <jyk102999@gmail.com>
2024-04-23 08:42:30 +0900

    🎉 :: project setting

---
+++ src/main/kotlin/com/killergram/killergrambackend/KillergramBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.killergram.killergrambackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KillergramBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<KillergramBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/killergram/killergrambackend/KillergramBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.killergram.killergrambackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KillergramBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
